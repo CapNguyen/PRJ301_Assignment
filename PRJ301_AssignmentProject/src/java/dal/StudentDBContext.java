29d08bc7fe962ec72b76659c4e1d82a7da4a558b
CapNguyen <118702737+CapNguyen@users.noreply.github.com>
2023-03-08 13:43:47 +0700

    Create DBContext
    Create connection to DB

---
+++ PRJ301_AssignmentProject/src/java/dal/StudentDBContext.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.util.ArrayList;
+import model.Student;
+
+/**
+ *
+ * @author admin
+ */
+public class StudentDBContext extends DBContext<Student> {
+
+    @Override
+    public void insert(Student model) {
+    }
+
+    @Override
+    public void update(Student model) {
+    }
+
+    @Override
+    public void delete(Student model) {
+    }
+
+    @Override
+    public Student get(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Student> all() {
+        return null;
+    }
+
+}
